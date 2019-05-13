183ea0e635043cfb1c4bcfc370ab3513d55addd9
Ayub <ayubali8668@icloud.com>
2019-05-13 04:20:05 -0400

    Initial Commit

---
+++ Trueu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trueu
+//
+//  Created by Ayub Ali on 2019-05-13.
+//  Copyright © 2019 Ayub Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
